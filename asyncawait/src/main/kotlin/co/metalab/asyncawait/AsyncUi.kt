52a60eca6f733bfec845dd7f4c95432d37729d00
Aleksey Masny <aleksey.masny@gmail.com>
2016-08-30 17:02:11 +0300

    Add documentation for public API (#2)
    * Add documentation for public API

--- asyncawait/src/main/kotlin/co/metalab/asyncawait/AsyncUi.kt
+++ asyncawait/src/main/kotlin/co/metalab/asyncawait/AsyncUi.kt
@@ -9,14 +9,56 @@ import java.util.concurrent.Executors
 
 private val executor = Executors.newSingleThreadExecutor()
 
+/**
+ * Run asynchronous computations based on [c] coroutine parameter.
+ * Must be used in UI thread.
+ *
+ * Execution starts immediately within the 'async' call and it runs until
+ * the first suspension point is reached ('await' call).
+ * Remaining part of coroutine will be executed after awaited task is completed
+ * and result is delivered into UI thread.
+ *
+ * @param c a coroutine representing asynchronous computations
+ *
+ * @return AsyncController object allowing to define optional `onError` handler
+ */
 fun async(coroutine c: AsyncController.() -> Continuation<Unit>): AsyncController {
    return async(c, AsyncController())
 }
 
+/**
+ * Run asynchronous computations based on [c] coroutine parameter.
+ * Prevents suspended coroutine to be resumed when [Activity] is in finishing state.
+ * Must be used in UI thread.
+ *
+ * Execution starts immediately within the 'async' call and it runs until
+ * the first suspension point is reached ('await' call).
+ * Remaining part of coroutine will be executed after awaited task is completed
+ * and [Activity] is not in finishing state. Result is delivered into UI thread.
+ *
+ * @param c a coroutine representing asynchronous computations
+ *
+ * @return AsyncController object allowing to define optional `onError` handler
+ */
 fun Activity.async(coroutine c: AsyncController.() -> Continuation<Unit>): AsyncController {
    return async(c, AsyncController(activity = this))
 }
 
+/**
+ * Run asynchronous computations based on [c] coroutine parameter.
+ * Prevents suspended coroutine to be resumed when [Fragment] is in invalid state.
+ * Must be used in UI thread.
+ *
+ * Execution starts immediately within the 'async' call and it runs until
+ * the first suspension point is reached ('await' call).
+ * Remaining part of coroutine will be executed after awaited task is completed
+ * and [Fragment] has parental [Activity] instance and it's in attached state.
+ * Result is delivered into UI thread.
+ *
+ * @param c a coroutine representing asynchronous computations
+ *
+ * @return AsyncController object allowing to define optional `onError` handler
+ */
 fun Fragment.async(coroutine c: AsyncController.() -> Continuation<Unit>): AsyncController {
    return async(c, AsyncController(fragment = this))
 }
@@ -31,6 +73,9 @@ typealias ErrorHandler = (Exception) -> Unit
 
 typealias ProgressHandler<P> = (P) -> Unit
 
+/**
+ * Controls coroutine execution and thread scheduling
+ */
 @AllowSuspendExtensions
 class AsyncController(private val activity: Activity? = null,
                       private val fragment: Fragment? = null) {
@@ -44,6 +89,16 @@ class AsyncController(private val activity: Activity? = null,
       }
    }
 
+   /**
+    * Non-blocking suspension point. Coroutine execution will proceed after [f] is finished
+    * in background thread.
+    *
+    * @param f a function to call in background thread. The result of [f] will be delivered
+    * into UI thread.
+    *
+    * @return the result of [f] delivered in UI thread after computation is done
+    * in background thread
+    */
    suspend fun <V> await(f: () -> V, machine: Continuation<V>) {
       executor.submit {
          try {
@@ -55,6 +110,19 @@ class AsyncController(private val activity: Activity? = null,
       }
    }
 
+   /**
+    * Non-blocking suspension point. Similar to [await] but its function [f] has functional parameter
+    * which can be called right in background thread in order to send progress value into
+    * [onProgress] progress handler.
+    *
+    * @param f a function to call in background thread. Its functional parameter
+    * `ProgressHandler<P>` can be called in background thread in order to send progress result.
+    *
+    * @param onProgress a function to handle progress result. Called in UI thread.
+    *
+    * @return the result of [f] delivered in UI thread after computation is done
+    * in background thread
+    */
    suspend fun <V, P> awaitWithProgress(f: (ProgressHandler<P>) -> V,
                                         onProgress: ProgressHandler<P>, machine: Continuation<V>) {
       executor.submit {
@@ -69,6 +137,11 @@ class AsyncController(private val activity: Activity? = null,
       }
    }
 
+   /**
+    * Optional error handler. Any exception happening in background thread
+    * will be delivered to this handler in UI thread. This handler has more priority than
+    * try/catch blocks around [await].
+    */
    fun onError(errorHandler: ErrorHandler) {
       this.errorHandler = errorHandler
    }
--- asyncawait/src/main/kotlin/co/metalab/asyncawait/RxExtensions.kt
+++ asyncawait/src/main/kotlin/co/metalab/asyncawait/RxExtensions.kt
@@ -2,6 +2,11 @@ package co.metalab.asyncawait
 
 import rx.Observable
 
+/**
+ * Waits in background until [observable] emits first value.
+ *
+ * @result the first value emitted by [observable]
+ */
 suspend fun <V> AsyncController.await(observable: Observable<V>, machine: Continuation<V>) {
    this.await({ observable.toBlocking().first() }, machine)
 }
