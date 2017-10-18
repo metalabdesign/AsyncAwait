91c5b943178e9f639fb57aa79bd4c18a3bb8b06a
pilgr <aleksey.masny@gmail.com>
2017-10-18 22:24:10 +0700

    Fix orangeTask to emulate long running task blocking running thread

--- app/src/main/kotlin/co/metalab/coroutinesample/KxOrangePresenter.kt
+++ app/src/main/kotlin/co/metalab/coroutinesample/KxOrangePresenter.kt
@@ -1,21 +1,21 @@
 package co.metalab.coroutinesample
 
 import android.util.Log
-import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.async
 
 class KxOrangePresenter(val orangeView: KxOrangeView) {
 
     suspend fun startLongRunningOrangeTask() {
         orangeView.setOrangeButtonText("Orange task in progress...")
-        val newButtonText = orangeTask()
+        val newButtonText = orangeTask().await()
         orangeView.setOrangeButtonText(newButtonText)
         Log.d("OrangePresenter", "Orange task result delivered into UI thread")
     }
 
-    suspend private fun orangeTask(): String {
-        delay(3000)
+    private fun orangeTask() = async {
+        Thread.sleep(3000)
         Log.d("OrangePresenter", "Orange task is done")
-        return "Orange task is done"
+        "Orange task is done"
     }
 
 }
